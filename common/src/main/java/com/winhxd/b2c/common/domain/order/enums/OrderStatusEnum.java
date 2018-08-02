f8d5b40205c8d306a50f009d23c9742cce527a58
Wang Bin <wangbin02@winchannel.com>
2018-08-02 16:09:07 +0800

    相关优化

--- common/src/main/java/com/winhxd/b2c/common/domain/order/enums/OrderStatusEnum.java
+++ common/src/main/java/com/winhxd/b2c/common/domain/order/enums/OrderStatusEnum.java
@@ -10,54 +10,55 @@ public enum OrderStatusEnum {
     /**
      * 已提交 只有微信在线支付有这个状态
      */
-    SUBMITTED(1, "已提交"),
-    /**
-     * 已接单
+    SUBMITTED((short)1, "订单已提交"),
+    
+     /**
+     * 订单待商铺确认
      */
-    RECEIVED(3, "已接单"),
+    RECEIVED((short)3, "订单待商铺确认"),
     /**
      * 待计价
      */
-    WAIT_QUOTA_VALUATION(5, "待计价"),
+    WAIT_QUOTA_VALUATION((short)5, "待商铺确认价格"),
     /**
-     * 已计价
+     * 商铺已确认价格，待用户确认
      */
-    ALREADY_QUOTA_VALUATION(7, "已计价"),
+    ALREADY_QUOTA_VALUATION((short)7, "商铺已确认价格，待用户确认"),
     /**
      * 待自提（已确认）
      */
-    WAIT_SELF_LIFTING(9, "待自提"),
+    WAIT_SELF_LIFTING((short)9, "订单待提取，取货码：{0}"),
     /**
      * 待顾客确认
      */
-    WAIT_CUSTOMER_CONFIRM(11, "待顾客确认"),
+    WAIT_CUSTOMER_CONFIRM((short)11, "待顾客确认"),
     /**
      * 已完成
      */
-    FINISHED(13, "已完成"),
+    FINISHED((short)22, "已完成"),
     /**
      * 已取消
      */
-    CANCELED(99, "已取消"),
+    CANCELED((short)99, "已取消"),
     /**
      * 已退款
      */
-    REFUNDED(77, "已取消"),
+    REFUNDED((short)77, "已退款"),
     /**
      * 待退款
      */
-    WAIT_REFUND(33, "待退款");
+    WAIT_REFUND((short)33, "待退款");
 
 
-    private int statusCode;
+    private short statusCode;
     private String statusDes;
 
-    OrderStatusEnum(int statusCode, String statusDes) {
+    OrderStatusEnum(short statusCode, String statusDes) {
         this.statusCode = statusCode;
         this.statusDes = statusDes;
     }
 
-    public int getStatusCode() {
+    public short getStatusCode() {
         return statusCode;
     }
 

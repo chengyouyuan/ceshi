6bef074924097823ee8f310c3cb207d9e9dc486e
wangbaokuo <wangbaokuo@winchannel.net>
2018-08-02 17:47:17 +0800

    购物车

---
+++ common/src/main/java/com/winhxd/b2c/common/domain/shopcar/model/ShopCar.java
@@ -0,0 +1,75 @@
+package com.winhxd.b2c.common.domain.shopcar.model;
+
+import java.util.Date;
+
+public class ShopCar {
+    private Long id;
+
+    private Long customerId;
+
+    private Long storeId;
+
+    private Long prodId;
+
+    private Integer prodNum;
+
+    private Date created;
+
+    private Date updated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Long storeId) {
+        this.storeId = storeId;
+    }
+
+    public Long getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(Long prodId) {
+        this.prodId = prodId;
+    }
+
+    public Integer getProdNum() {
+        return prodNum;
+    }
+
+    public void setProdNum(Integer prodNum) {
+        this.prodNum = prodNum;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+}

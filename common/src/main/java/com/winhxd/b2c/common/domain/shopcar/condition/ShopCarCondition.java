dca4666caf6b225ea351bc9b8119073130f97af4
wangbaokuo <wangbaokuo@winchannel.net>
2018-08-03 15:05:55 +0800

    购物车接口调整

--- common/src/main/java/com/winhxd/b2c/common/domain/shopcar/condition/ShopCarCondition.java
+++ common/src/main/java/com/winhxd/b2c/common/domain/shopcar/condition/ShopCarCondition.java
@@ -9,11 +9,12 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 /**
- * @auther: wangbaokuo
+ * @author: wangbaokuo
  * @date: 2018/8/3 09:29
  * @description:
  */
 @ApiModel("用户加购传参")
+@Data
 public class ShopCarCondition extends BaseCondition{
 
     @ApiModelProperty(value = "门店ID")
@@ -31,8 +32,8 @@ public class ShopCarCondition extends BaseCondition{
     @ApiModelProperty(value = "商品数量")
     private Integer prodNum;
 
-    @ApiModelProperty(value = "支付方式(1:微信在线付款,2微信扫码付款)")
-    private Byte payType;
+    @ApiModelProperty(value = "支付方式(1:微信扫码付款,2微信在线付款)")
+    private Short payType;
 
     @ApiModelProperty(value = "优惠券ID")
     private Long couponId;
@@ -43,75 +44,4 @@ public class ShopCarCondition extends BaseCondition{
     @ApiModelProperty(value = "金额")
     private BigDecimal orderTotalMoney;
 
-    public Long getStoreId() {
-        return storeId;
-    }
-
-    public void setStoreId(Long storeId) {
-        this.storeId = storeId;
-    }
-
-    public String getExtractAddress() {
-        return extractAddress;
-    }
-
-    public void setExtractAddress(String extractAddress) {
-        this.extractAddress = extractAddress;
-    }
-
-    public Date getExtractTime() {
-        return extractTime;
-    }
-
-    public void setExtractTime(Date extractTime) {
-        this.extractTime = extractTime;
-    }
-
-    public String getSkuCode() {
-        return skuCode;
-    }
-
-    public void setSkuCode(String skuCode) {
-        this.skuCode = skuCode;
-    }
-
-    public Integer getProdNum() {
-        return prodNum;
-    }
-
-    public void setProdNum(Integer prodNum) {
-        this.prodNum = prodNum;
-    }
-
-    public Byte getPayType() {
-        return payType;
-    }
-
-    public void setPayType(Byte payType) {
-        this.payType = payType;
-    }
-
-    public Long getCouponId() {
-        return couponId;
-    }
-
-    public void setCouponId(Long couponId) {
-        this.couponId = couponId;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public BigDecimal getOrderTotalMoney() {
-        return orderTotalMoney;
-    }
-
-    public void setOrderTotalMoney(BigDecimal orderTotalMoney) {
-        this.orderTotalMoney = orderTotalMoney;
-    }
 }
--- common/src/main/java/com/winhxd/b2c/common/domain/shopcar/vo/ShopCarVO.java
+++ common/src/main/java/com/winhxd/b2c/common/domain/shopcar/vo/ShopCarVO.java
@@ -1,9 +1,60 @@
 package com.winhxd.b2c.common.domain.shopcar.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
- * @auther: wangbaokuo
+ * @author: wangbaokuo
  * @date: 2018/8/3 10:12
  * @description:
  */
+@ApiModel("购物车")
+@Data
 public class ShopCarVO {
+
+    @ApiModelProperty(value = "购物车ID")
+    private Long id;
+
+    @ApiModelProperty(value = "用户ID")
+    private Long customerId;
+
+    @ApiModelProperty(value = "门店ID")
+    private Long storeId;
+
+    @ApiModelProperty(value = "商品sku")
+    private String skuCode;
+
+    @ApiModelProperty(value = "商品数量")
+    private Integer prodNum;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date created;
+
+    @ApiModelProperty(value = "修改时间")
+    private Date updated;
+
+    @ApiModelProperty(value = "创建人")
+    private Long createdby;
+
+    @ApiModelProperty(value = "更新人")
+    private Long updatedby;
+
+    @ApiModelProperty(value = "库存状态(0无货,1有货)")
+    private Byte prodRepertory;
+
+    @ApiModelProperty(value = "上下架状态(0下架,1上架)")
+    private Byte prodStatus;
+
+    @ApiModelProperty(value = "商品图片")
+    private String prodImg;
+
+    @ApiModelProperty(value = "商品名称")
+    private String prodName;
+
+    @ApiModelProperty(value = "商品单价")
+    private BigDecimal prodPrice;
 }

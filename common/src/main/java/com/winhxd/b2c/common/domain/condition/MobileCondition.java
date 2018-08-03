56a0d406b86088411ea372f04adf9b0b765d9acd
lvsen <lvsen@winchannel.net>
2018-08-03 15:15:35 +0800

    加moblieCondition

--- common/src/main/java/com/winhxd/b2c/common/domain/condition/MobileCondition.java
+++ common/src/main/java/com/winhxd/b2c/common/domain/condition/MobileCondition.java
@@ -20,23 +20,11 @@ public class MobileCondition implements Serializable {
     @ApiModelProperty(value = "did号")
     private String did;
 
-    @ApiModelProperty(value = "wifi")
-    private String wifi;
-
     @ApiModelProperty(value = "租户ID")
     private String grp;
 
     @ApiModelProperty(value = "门店下单时所用设备")
     private String imei;
-
-    @ApiModelProperty(value = "维度")
-    private String lat;
-
-    @ApiModelProperty(value = "经度")
-    private String lon;
-
-    @ApiModelProperty(value = "坐标类型经度")
-    private String coordinateType;
 
     @ApiModelProperty(value = "src")
     private String src;
@@ -46,9 +34,6 @@ public class MobileCondition implements Serializable {
 
     @ApiModelProperty(value = "lang")
     private String lang;
-
-    @ApiModelProperty(value = "token")
-    private String token;
 
     @ApiModelProperty(value = "安卓模拟器ID，用于记录日志使用")
     private String emulatordid;
@@ -62,14 +47,6 @@ public class MobileCondition implements Serializable {
 
     public void setPlatform(String platform) {
         this.platform = platform;
-    }
-
-    public String getWifi() {
-        return wifi;
-    }
-
-    public void setWifi(String wifi) {
-        this.wifi = wifi;
     }
 
     public String getGrp() {
@@ -118,38 +95,6 @@ public class MobileCondition implements Serializable {
 
     public void setDid(String did) {
         this.did = did;
-    }
-
-    public String getLat() {
-        return lat;
-    }
-
-    public void setLat(String lat) {
-        this.lat = lat;
-    }
-
-    public String getLon() {
-        return lon;
-    }
-
-    public void setLon(String lon) {
-        this.lon = lon;
-    }
-
-    public String getCoordinateType() {
-        return coordinateType;
-    }
-
-    public void setCoordinateType(String coordinateType) {
-        this.coordinateType = coordinateType;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 
     public String getEmulatordid() {

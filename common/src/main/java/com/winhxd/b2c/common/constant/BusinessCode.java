86256a3f8345907a4df629d4997261d3b7648736
chengyouyuan <984415140@qq.com>
2018-08-03 15:49:54 +0800

    修复删除的常量

--- common/src/main/java/com/winhxd/b2c/common/constant/BusinessCode.java
+++ common/src/main/java/com/winhxd/b2c/common/constant/BusinessCode.java
@@ -11,6 +11,18 @@ public final class BusinessCode {
     /** 登录凭证无效 */
     public static final int CODE_1002 = 1002;
 
+    /** 没有权限 */
+    public static final int CODE_1003 = 1003;
+    /** 账号无效 */
+    public static final int CODE_1004 = 1004;
+    /** 密码错误 */
+    public static final int CODE_1005 = 1005;
+    /**  账号未启用 */
+    public static final int CODE_1006 = 1006;
+    /** 参数无效 */
+    public static final int CODE_1007 = 1007;
+
+
     /** 原密码输入错误 */
     public static final int CODE_301201 = 301201;
     /** 新密码与原密码相同 */

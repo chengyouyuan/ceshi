d47c58f79904e753c7f98d424060f88becffac3d
chengyouyuan <984415140@qq.com>
2018-08-04 15:01:04 +0800

    在message-service中添加生成小程序二维码的api接口

---
+++ service-projects/message-service/src/main/java/com/winhxd/b2c/message/service/WechatShareService.java
@@ -0,0 +1,20 @@
+package com.winhxd.b2c.message.service;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * @Description: 微信分享业务接口类
+ * @author chengyy
+ * @date 2018/8/4 13:33
+ */
+public interface WechatShareService {
+    /**
+     * @author chengyy
+     * @date 2018/8/4 13:35
+     * @Description 生成微信小程序二维码
+     * @param  storeUserId 当前的门店id
+     * @return 图片二进制数据
+     */
+    byte[] generateQRCodePic(Long storeUserId);
+}

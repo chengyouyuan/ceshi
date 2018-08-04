5fd0caf376365a768940faa51ddeb210e3d177fa
pangjianhua <pangjianhua@winchannel.net>
2018-08-04 10:41:44 +0800

    整理代码

--- service-projects/order-service/src/main/java/com/winhxd/b2c/order/api/ApiOrderController.java
+++ service-projects/order-service/src/main/java/com/winhxd/b2c/order/api/ApiOrderController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Api(tags = "ApiOrder")
-@RequestMapping(value = "")
+@RequestMapping(value = "/api-order/order")
 public class ApiOrderController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApiOrderController.class);
 
@@ -32,18 +32,18 @@ public class ApiOrderController {
             @ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部异常"),
             @ApiResponse(code = BusinessCode.CODE_1002, message = "登录凭证无效")
     })
-    @RequestMapping(value = "/api-order/api/order/422/v1/handleOrderRefundByStore", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseResult<Boolean> handleOrderRefundByStore(@RequestBody OrderRefundCondition OrderRefundCondition) {
-        LOGGER.info("=/api-order/api/order/421/v1/orderRefundByCustomer-B端退款订单处理接口=--开始--{}");
+    @RequestMapping(value = "/422/v1/handleOrderRefundByStore", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseResult<Boolean> handleOrderRefundByStore(@RequestBody OrderRefundCondition orderRefundCondition) {
+        LOGGER.info("=/api-order/order/421/v1/orderRefundByCustomer-B端退款订单处理接口=--开始--{}");
         ResponseResult<Boolean> result = new ResponseResult<>();
         try {
             //返回对象
             result.setData(null);
         } catch (Exception e) {
-            LOGGER.error("=/api-order/api/order/421/v1/orderRefundByCustomer-B端退款订单处理接口=--异常" + e.getMessage(), e);
+            LOGGER.error("=/api-order/order/421/v1/orderRefundByCustomer-B端退款订单处理接口=--异常" + e.getMessage(), e);
             result.setCode(BusinessCode.CODE_1001);
         }
-        LOGGER.info("=/api-order/api/order/421/v1/orderRefundByCustomer-B端退款订单处理接口=--结束 result={}", result);
+        LOGGER.info("=/api-order/order/421/v1/orderRefundByCustomer-B端退款订单处理接口=--结束 result={}", result);
         return result;
     }
 
@@ -52,18 +52,18 @@ public class ApiOrderController {
             @ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部异常"),
             @ApiResponse(code = BusinessCode.CODE_1002, message = "登录凭证无效")
     })
-    @RequestMapping(value = "/api-order/api/order/421/v1/orderCancel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseResult<Boolean> orderRefundByCustomer(@RequestBody OrderRefundCondition OrderRefundCondition) {
-        LOGGER.info("=/api-order/api/order/421/v1/orderRefundByCustomer-C端订单退款接口=--开始--{}");
+    @RequestMapping(value = "/421/v1/orderCancel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseResult<Boolean> orderRefundByCustomer(@RequestBody OrderRefundCondition orderRefundCondition) {
+        LOGGER.info("=/api-order/order/421/v1/orderRefundByCustomer-C端订单退款接口=--开始--{}");
         ResponseResult<Boolean> result = new ResponseResult<>();
         try {
             //返回对象
             result.setData(null);
         } catch (Exception e) {
-            LOGGER.error("=/api-order/api/order/421/v1/orderRefundByCustomer-C端订单退款接口=--异常" + e.getMessage(), e);
+            LOGGER.error("=/api-order/order/421/v1/orderRefundByCustomer-C端订单退款接口=--异常" + e.getMessage(), e);
             result.setCode(BusinessCode.CODE_1001);
         }
-        LOGGER.info("=/api-order/api/order/421/v1/orderRefundByCustomer-C端订单退款接口=--结束 result={}", result);
+        LOGGER.info("=/api-order/order/421/v1/orderRefundByCustomer-C端订单退款接口=--结束 result={}", result);
         return result;
     }
 
@@ -72,19 +72,19 @@ public class ApiOrderController {
             @ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部异常"),
             @ApiResponse(code = BusinessCode.CODE_1002, message = "登录凭证无效")
     })
-    @RequestMapping(value = "/api-order/api/order/420/v1/orderCancel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/420/v1/orderCancel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseResult<OrderInfoListVO> orderCancel(@RequestBody OrderCancelCondition orderCancelCondition) {
-        LOGGER.info("=/api-order/api/order/420/v1/orderCancel订单取消接口=--开始--{}");
+        LOGGER.info("=/api-order/order/420/v1/orderCancel订单取消接口=--开始--{}");
         Long customerId = 1L;
         ResponseResult<OrderInfoListVO> result = new ResponseResult<>();
         try {
             //返回对象
             result.setData(null);
         } catch (Exception e) {
-            LOGGER.error("=/api-order/api/order/420/v1/orderCancel订单取消接口=--异常" + e.getMessage(), e);
+            LOGGER.error("=/api-order/order/420/v1/orderCancel订单取消接口=--异常" + e.getMessage(), e);
             result.setCode(BusinessCode.CODE_1001);
         }
-        LOGGER.info("=/api-order/api/order/420/v1/orderCancel订单取消接口=--结束 result={}", result);
+        LOGGER.info("=/api-order/order/420/v1/orderCancel订单取消接口=--结束 result={}", result);
         return result;
     }
 }

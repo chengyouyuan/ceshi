114fd33af33cf3192f7312ed1210872c7ca178aa
chengyouyuan <984415140@qq.com>
2018-08-06 15:52:54 +0800

    添加更新用户状态接口（添加黑名单移除黑名单） 添加查询用户订单信息的接口（未完善TODO） 添加根据用户id批量查询接口

--- common/src/main/java/com/winhxd/b2c/common/feign/customer/CustomerServiceClient.java
+++ common/src/main/java/com/winhxd/b2c/common/feign/customer/CustomerServiceClient.java
@@ -10,41 +10,55 @@ import feign.hystrix.FallbackFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 /**
- * @Description: 用户服务接口
  * @author chengyy
+ * @Description: 用户服务接口
  * @date 2018/8/4 16:56
  */
-@FeignClient(value = ServiceName.CUSTOMER_SERVICE,fallbackFactory = CustomerServiceClientFallBack.class)
+@FeignClient(value = ServiceName.CUSTOMER_SERVICE, fallbackFactory = CustomerServiceClientFallBack.class)
 public interface CustomerServiceClient {
-     /**
-      * @author chengyy
-      * @date 2018/8/4 17:59
-      * @Description 根据条件查询后台用户列表页数据，分页查询
-      * @param  condition 查询条件对象
-      * @return  分页数据
-      */
-     @RequestMapping(value="/customer/2005/v1/queryCustomerPageInfo",method = RequestMethod.POST)
-     ResponseResult<PagedList<CustomerUserInfoVO1>> queryCustomerPageInfo(@RequestBody CustomerUserInfoCondition1 condition);
+    /**
+     * @param condition 查询条件对象
+     * @return 分页数据
+     * @author chengyy
+     * @date 2018/8/4 17:59
+     * @Description 根据条件查询后台用户列表页数据，分页查询
+     */
+    @RequestMapping(value = "/customer/2005/v1/queryCustomerPageInfo", method = RequestMethod.POST)
+    ResponseResult<PagedList<CustomerUserInfoVO1>> queryCustomerPageInfo(@RequestBody CustomerUserInfoCondition1 condition);
 
-     /**
-      * @author chengyy
-      * @date 2018/8/6 13:32
-      * @Description 更新用户的状态
-      * @param condition 参数条件对象
-      * @return   null
-      */
-     @RequestMapping(value = "/customer/2006/v1/updateStatus",method = RequestMethod.POST)
-     ResponseResult<Void> updateStatus(@RequestBody CustomerUserInfoCondition1 condition);
+    /**
+     * @param condition 参数条件对象
+     * @return null
+     * @author chengyy
+     * @date 2018/8/6 13:32
+     * @Description 更新用户的状态
+     */
+    @RequestMapping(value = "/customer/2006/v1/updateStatus", method = RequestMethod.POST)
+    ResponseResult<Void> updateStatus(@RequestBody CustomerUserInfoCondition1 condition);
+
+    /**
+     * @param ids 用户id
+     * @return 用户信息
+     * @author chengyy
+     * @date 2018/8/6 15:27
+     * @Description 批量查询用户信息
+     */
+    @RequestMapping(value = "/customer/2007/v1/findCustomerUserByIds", method = RequestMethod.POST)
+    ResponseResult<List<CustomerUserInfoVO1>> findCustomerUserByIds(@RequestBody List<Long> ids);
 }
 
-class CustomerServiceClientFallBack implements CustomerServiceClient,FallbackFactory<CustomerServiceClient>{
+class CustomerServiceClientFallBack implements CustomerServiceClient, FallbackFactory<CustomerServiceClient> {
     Logger logger = LoggerFactory.getLogger(CustomerServiceClientFallBack.class);
     Throwable throwable;
+
     @Override
     public CustomerServiceClient create(Throwable throwable) {
         this.throwable = throwable;
@@ -60,6 +74,12 @@ class CustomerServiceClientFallBack implements CustomerServiceClient,FallbackFac
     @Override
     public ResponseResult<Void> updateStatus(CustomerUserInfoCondition1 condition) {
         logger.error("CustomerServiceClientFallBack -> updateStatus错误信息{}", throwable.getMessage());
+        return new ResponseResult<>(BusinessCode.CODE_1001);
+    }
+
+    @Override
+    public ResponseResult<List<CustomerUserInfoVO1>> findCustomerUserByIds(List<Long> ids) {
+        logger.error("CustomerServiceClientFallBack -> findCustomerUserByIds错误信息{}", throwable.getMessage());
         return new ResponseResult<>(BusinessCode.CODE_1001);
     }
 }

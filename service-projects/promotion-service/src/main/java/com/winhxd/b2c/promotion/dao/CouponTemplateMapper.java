527dbd535bfd9f73f10baed40b60702f59a6658c
wanglei <wanglei02@winchannel.net>
2018-08-06 20:26:52 +0800

    优惠券模板分页列表查询

--- service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/dao/CouponTemplateMapper.java
+++ service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/dao/CouponTemplateMapper.java
@@ -3,6 +3,7 @@ package com.winhxd.b2c.promotion.dao;
 import com.winhxd.b2c.common.domain.promotion.condition.CouponTemplateCondition;
 import com.winhxd.b2c.common.domain.promotion.model.CouponTemplate;
 import com.winhxd.b2c.common.domain.promotion.vo.CouponTemplateVO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -21,5 +22,5 @@ public interface CouponTemplateMapper {
 
     CouponTemplate selectCouponTemplateById(long id);
 
-    List<CouponTemplateVO> getCouponTemplatePageByCondition(CouponTemplateCondition couponTemplateCondition);
+    List<CouponTemplateVO> getCouponTemplatePageByCondition(@Param("condition")CouponTemplateCondition condition);
 }

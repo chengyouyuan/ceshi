37c82cfb6bcc622472f8d5df1dffc227148051ac
chengyouyuan <984415140@qq.com>
2018-08-06 11:36:02 +0800

    在用户表中添加status状态字段，1位有效0位无效 添加内部用户列表查询接口 添加用户列表数据导出接口 添加execl数据导出的util类

---
+++ common/src/main/java/com/winhxd/b2c/common/domain/system/login/condition/CustomerUserInfoCondition1.java
@@ -0,0 +1,35 @@
+package com.winhxd.b2c.common.domain.system.login.condition;
+
+import com.winhxd.b2c.common.domain.base.condition.BaseCondition;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Description: 后台用户列表页请求参数
+ * @author chengyy
+ * @date 2018/8/4 17:56
+ */
+@Data
+@ApiModel(value = "用户请求参数",description = "后台用户列表请求参数")
+public class CustomerUserInfoCondition1 extends BaseCondition {
+    /**用户账号*/
+    @ApiModelProperty("用户账号")
+    private String customerMobile;
+
+    /**微信openId*/
+    @ApiModelProperty("微信openId")
+    private String openId;
+
+    /**昵称*/
+    @ApiModelProperty("昵称")
+    private String nickName;
+
+    @ApiModelProperty("门店名称")
+    private String storeName;
+
+    /**用户状态,默认有效true，无效(黑名单)false,默认是有效*/
+    @ApiModelProperty("用户状态,默认有效1，无效(黑名单)0,默认是有效")
+    private Integer status = 1;
+
+}

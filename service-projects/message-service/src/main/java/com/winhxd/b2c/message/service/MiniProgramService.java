bd04f9d5782b639ad306e611b2f6e7623eb64f1b
jujinbiao <jujinbiao@winchannel.net>
2018-08-06 16:50:34 +0800

    消息服务

---
+++ service-projects/message-service/src/main/java/com/winhxd/b2c/message/service/MiniProgramService.java
@@ -0,0 +1,19 @@
+package com.winhxd.b2c.message.service;
+
+import com.winhxd.b2c.common.domain.ResponseResult;
+import com.winhxd.b2c.common.domain.message.model.MiniOpenId;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author jujinbiao
+ * @className MiniProgramService 小程序接口
+ * @description
+ */
+public interface MiniProgramService {
+    /**
+     * 根据code获取小程序的openid和session_key
+     * @param code
+     * @return
+     */
+    ResponseResult<MiniOpenId> getMiniOpenId(String code);
+}

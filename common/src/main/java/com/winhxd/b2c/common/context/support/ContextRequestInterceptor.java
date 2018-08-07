f9bfbc251088a801843163f948665fc7e1e10de2
lixiaodong <lixiaodong@winchannel.net>
2018-08-07 18:37:52 +0800

    网关支持token校验

--- common/src/main/java/com/winhxd/b2c/common/context/support/ContextRequestInterceptor.java
+++ common/src/main/java/com/winhxd/b2c/common/context/support/ContextRequestInterceptor.java
@@ -4,12 +4,8 @@ import com.winhxd.b2c.common.context.AdminUser;
 import com.winhxd.b2c.common.context.CustomerUser;
 import com.winhxd.b2c.common.context.StoreUser;
 import com.winhxd.b2c.common.context.UserContext;
-import com.winhxd.b2c.common.util.JsonUtil;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 public class ContextRequestInterceptor implements RequestInterceptor {
     @Override
@@ -18,23 +14,13 @@ public class ContextRequestInterceptor implements RequestInterceptor {
         CustomerUser customerUser = UserContext.getCurrentCustomerUser();
         StoreUser storeUser = UserContext.getCurrentStoreUser();
         if (adminUser != null) {
-            requestTemplate.header(UserContext.HEADER_USER_ADMIN, getHeaderJsonString(adminUser));
+            requestTemplate.header(UserContext.HEADER_USER_ADMIN, ContextHelper.getHeaderJsonString(adminUser));
         }
         if (customerUser != null) {
-            requestTemplate.header(UserContext.HEADER_USER_CUSTOMER, getHeaderJsonString(customerUser));
+            requestTemplate.header(UserContext.HEADER_USER_CUSTOMER, ContextHelper.getHeaderJsonString(customerUser));
         }
         if (storeUser != null) {
-            requestTemplate.header(UserContext.HEADER_USER_STORE, getHeaderJsonString(storeUser));
-        }
-    }
-
-
-    public static String getHeaderJsonString(Object obj) {
-        String json = JsonUtil.toJSONString(obj);
-        try {
-            return URLEncoder.encode(json, "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            return null;
+            requestTemplate.header(UserContext.HEADER_USER_STORE, ContextHelper.getHeaderJsonString(storeUser));
         }
     }
 }

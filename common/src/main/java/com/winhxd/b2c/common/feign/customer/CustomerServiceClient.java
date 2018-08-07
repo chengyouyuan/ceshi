f7485f7e44d0437fb95cc052807a378911d0c039
chengyouyuan <984415140@qq.com>
2018-08-07 15:30:58 +0800

    删除多余的注解

--- common/src/main/java/com/winhxd/b2c/common/feign/customer/CustomerServiceClient.java
+++ common/src/main/java/com/winhxd/b2c/common/feign/customer/CustomerServiceClient.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;

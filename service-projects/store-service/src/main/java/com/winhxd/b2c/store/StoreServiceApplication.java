2e86fb7d583a9a67350945fb45849168d9f60321
chengyouyuan <984415140@qq.com>
2018-08-07 15:00:39 +0800

    添加开启fegin扫描注解

--- service-projects/store-service/src/main/java/com/winhxd/b2c/store/StoreServiceApplication.java
+++ service-projects/store-service/src/main/java/com/winhxd/b2c/store/StoreServiceApplication.java
@@ -4,6 +4,7 @@ import com.winhxd.b2c.common.config.ServiceConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Import;
 @MapperScan("com.winhxd.b2c.store.dao")
 @ComponentScan(basePackages = "com.winhxd.b2c")
 @Import(ServiceConfig.class)
+@EnableFeignClients(basePackages = "com.winhxd.b2c.common.feign")
 public class StoreServiceApplication {
 
     public static void main(String[] args) {

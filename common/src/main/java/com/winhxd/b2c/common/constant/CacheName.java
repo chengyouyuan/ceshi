7c46461978eddc137fea550c5ac63f68f8ff82d6
wufuyun <wufuyun@winchannel.net>
2018-08-07 15:12:15 +0800

    B端用户C端用户登录更新

--- common/src/main/java/com/winhxd/b2c/common/constant/CacheName.java
+++ common/src/main/java/com/winhxd/b2c/common/constant/CacheName.java
@@ -28,6 +28,13 @@ public class CacheName {
      * 订单修改锁KEY 全局唯一，订单修改统一用这个
      */
     public static final String CACHE_KEY_STORE_ORDER_SALESSUMMARY = "STORE:SALESSUMMARY:";
+    /**
+     * B端用户登录key
+     */
+    public static final String STORE_USER_INFO_TOKEN = "STORE_USER_INFO_TOKEN:";
+    /**
+     * C端用户登录key
+     */
+    public static final String CUSTOMER_USER_INFO_TOKEN = "CUSTOMER_USER_INFO_TOKEN:";
 
-    
 }

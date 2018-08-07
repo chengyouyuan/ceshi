d429f3ef3501a7c17bb5ed0e486069706215b92a
zhuchongchen <zhuchongchen@winchannel.net>
2018-08-07 21:06:01 +0800

    惠小店 小程序 商品中心

--- common/src/main/java/com/winhxd/b2c/common/feign/product/ProductServiceClient.java
+++ common/src/main/java/com/winhxd/b2c/common/feign/product/ProductServiceClient.java
@@ -26,23 +26,71 @@ import feign.hystrix.FallbackFactory;
 @FeignClient(value = ServiceName.PRODUCT_SERVICE, fallbackFactory = ProductServiceFallback.class)
 public interface ProductServiceClient {
 
-	 @RequestMapping(value = "/product/v1/getProductMsg/", method = RequestMethod.POST)
-	 ResponseResult<ProductMsgVO> getProductMsg(@RequestBody ProductConditionByPage condition);
-	 
-	 @RequestMapping(value = "/product/v1/getProductSkuMsg/", method = RequestMethod.POST)
-	 ResponseResult<ProductSkuMsgVO> getProductSkuMsg(ProductConditionByPage condition);
-	 
-	 @RequestMapping(value = "/product/v1/getProductsByPage/", method = RequestMethod.POST)
-	 ResponseResult<PagedList<ProductVO>> getProductsByPage(@RequestBody ProductConditionByPage condition);
-	 
-	 @RequestMapping(value = "/product/v1/getProductSkusByPage/", method = RequestMethod.POST)
-	 ResponseResult<PagedList<ProductSkuVO>> getProductSkusByPage(@RequestBody ProductConditionByPage condition);
-	 
-	 @RequestMapping(value = "/product/v1/getProducts/", method = RequestMethod.POST)
-	 ResponseResult<List<ProductVO>> getProducts(@RequestBody ProductCondition condition);
-	 
-	 @RequestMapping(value = "/product/v1/getProductSkus/", method = RequestMethod.POST)
-	 ResponseResult<List<ProductSkuVO>> getProductSkus(@RequestBody ProductCondition condition);
+	/**
+	 * 获取分类、 品牌、 商品spu信息（分页）
+	 * @Title: getProductMsg
+	 * @param: @param condition
+	 * @param: @return      
+	 * @return: ResponseResult<ProductMsgVO>      
+	 * @throws
+	 */
+	@RequestMapping(value = "/product/v1/getProductMsg/", method = RequestMethod.POST)
+	ResponseResult<ProductMsgVO> getProductMsg(@RequestBody ProductConditionByPage condition);
+	
+	/**
+	 * 获取一级分类、二级分类、商品sku信息（分页）
+	 * @Title: getProductSkuMsg
+	 * @param: @param condition
+	 * @param: @return      
+	 * @return: ResponseResult<ProductSkuMsgVO>      
+	 * @throws
+	 */
+	@RequestMapping(value = "/product/v1/getProductSkuMsg/", method = RequestMethod.POST)
+	ResponseResult<ProductSkuMsgVO> getProductSkuMsg(ProductConditionByPage condition);
+	
+	/**
+	 * 获取商品spu信息 （分页）
+	 * @Title: getProductsByPage
+	 * @param: @param condition
+	 * @param: @return      
+	 * @return: ResponseResult<PagedList<ProductVO>>      
+	 * @throws
+	 */
+	@RequestMapping(value = "/product/v1/getProductsByPage/", method = RequestMethod.POST)
+	ResponseResult<PagedList<ProductVO>> getProductsByPage(@RequestBody ProductConditionByPage condition);
+	
+	/**
+	 * 获取商品sku信息 （分页）
+	 * @Title: getProductSkusByPage
+	 * @param: @param condition
+	 * @param: @return      
+	 * @return: ResponseResult<PagedList<ProductSkuVO>>      
+	 * @throws
+	 */
+	@RequestMapping(value = "/product/v1/getProductSkusByPage/", method = RequestMethod.POST)
+	ResponseResult<PagedList<ProductSkuVO>> getProductSkusByPage(@RequestBody ProductConditionByPage condition);
+	
+	/**
+	 * 获取商品spu信息
+	 * @Title: getProducts
+	 * @param: @param condition
+	 * @param: @return      
+	 * @return: ResponseResult<List<ProductVO>>      
+	 * @throws
+	 */
+	@RequestMapping(value = "/product/v1/getProducts/", method = RequestMethod.POST)
+	ResponseResult<List<ProductVO>> getProducts(@RequestBody ProductCondition condition);
+	
+	/**
+	 * 获取商品sku信息
+	 * @Title: getProductSkus
+	 * @param: @param condition
+	 * @param: @return      
+	 * @return: ResponseResult<List<ProductSkuVO>>      
+	 * @throws
+	 */
+	@RequestMapping(value = "/product/v1/getProductSkus/", method = RequestMethod.POST)
+	ResponseResult<List<ProductSkuVO>> getProductSkus(@RequestBody ProductCondition condition);
 	 
 }
 

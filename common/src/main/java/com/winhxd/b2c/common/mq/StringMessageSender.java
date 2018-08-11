8f390e9d9541276a8812dff72fb9188c56fac132
Wang Bin <wangbin02@winchannel.com>
2018-08-11 09:34:22 +0800

    MQ消息相关调整

--- common/src/main/java/com/winhxd/b2c/common/mq/StringMessageSender.java
+++ common/src/main/java/com/winhxd/b2c/common/mq/StringMessageSender.java
@@ -1,15 +1,14 @@
 package com.winhxd.b2c.common.mq;
 
-import com.winhxd.b2c.common.util.JsonUtil;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
+import com.winhxd.b2c.common.util.JsonUtil;
 
 /**
  * @author lixiaodong
  */
-@Component
 public class StringMessageSender {
     @Autowired
     private AmqpTemplate amqpTemplate;

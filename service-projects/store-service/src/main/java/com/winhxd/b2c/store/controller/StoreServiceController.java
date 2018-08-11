d9283db7b804070d7493fc32bdb30ff50c673c3d
lvsen <lvsen@winchannel.net>
2018-08-11 10:46:26 +0800

    删除无用接口

--- service-projects/store-service/src/main/java/com/winhxd/b2c/store/controller/StoreServiceController.java
+++ service-projects/store-service/src/main/java/com/winhxd/b2c/store/controller/StoreServiceController.java
@@ -171,20 +171,6 @@ public class StoreServiceController implements StoreServiceClient {
 	}
 
 	@Override
-	public ResponseResult<Void> statisticsStoreProdInfo(StoreProductManageCondition condition) {
-		if (condition != null) {
-			logger.error("StoreServiceController -> statisticsStoreProdInfo获取的参数异常！");
-			throw new BusinessException(BusinessCode.CODE_1007);
-		}
-		if (null != condition.getStoreId() || StringUtils.isNotBlank(condition.getProdId()) || null != condition.getUpdatedBy()) {
-			logger.error("StoreServiceController -> statisticsStoreProdInfo获取的参数异常！");
-			throw new BusinessException(BusinessCode.CODE_1007);
-		}
-		int flag = storeProductStatisticsService.modifyQuantitySoldOutByStoreIdAndProdId(condition);
-		return new ResponseResult<>();
-	}
-
-	@Override
 	public ResponseResult<StoreUserInfoVO> findStoreUserInfoByCustomerId(@RequestParam("customerUserId")Long customerUserId) {
     	ResponseResult<StoreUserInfoVO> responseResult = new ResponseResult<>();
 		if(customerUserId == null) {

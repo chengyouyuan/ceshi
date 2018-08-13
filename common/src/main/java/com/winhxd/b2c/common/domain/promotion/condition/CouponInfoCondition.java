f58e135e450318e17441588e613f3bdf3060842e
liuhanning <liuhanning@winchannel.net>
2018-08-13 10:01:06 +0800

    优惠券详情condition

--- common/src/main/java/com/winhxd/b2c/common/domain/promotion/condition/CouponInfoCondition.java
+++ common/src/main/java/com/winhxd/b2c/common/domain/promotion/condition/CouponInfoCondition.java
@@ -1,5 +1,7 @@
 package com.winhxd.b2c.common.domain.promotion.condition;
 
+import com.winhxd.b2c.common.domain.common.ApiCondition;
+
 import io.swagger.annotations.ApiModelProperty;
 
 /**
@@ -7,7 +9,7 @@ import io.swagger.annotations.ApiModelProperty;
  * @Date 2018/8/6 10 56
  * @Description
  */
-public class CouponInfoCondition  extends CommonCondition {
+public class CouponInfoCondition  extends ApiCondition {
     @ApiModelProperty(value = "用户id")
     private Long customerId;
 

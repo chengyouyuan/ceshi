8fa6490c38c90f47a9960c2a39aa346bef061b2a
wangxiaoshun <wangxiaoshun@winchannel.net>
2018-08-13 10:01:16 +0800

    继承ApiCondition

--- common/src/main/java/com/winhxd/b2c/common/domain/promotion/condition/CouponInfoCondition.java
+++ common/src/main/java/com/winhxd/b2c/common/domain/promotion/condition/CouponInfoCondition.java
@@ -1,7 +1,5 @@
 package com.winhxd.b2c.common.domain.promotion.condition;
 
-import com.winhxd.b2c.common.domain.common.ApiCondition;
-
 import io.swagger.annotations.ApiModelProperty;
 
 /**
@@ -9,7 +7,7 @@ import io.swagger.annotations.ApiModelProperty;
  * @Date 2018/8/6 10 56
  * @Description
  */
-public class CouponInfoCondition  extends ApiCondition {
+public class CouponInfoCondition  extends CommonCondition {
     @ApiModelProperty(value = "用户id")
     private Long customerId;
 

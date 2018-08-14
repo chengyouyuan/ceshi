6e2e0eaf200b1e81e52a958c68293a6ecf279a85
lixiaodong <lixiaodong@winchannel.net>
2018-08-14 18:13:13 +0800

    增加网关日志记录

--- base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/filter/GatewayFilter.java
+++ base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/filter/GatewayFilter.java
@@ -112,6 +112,7 @@ public class GatewayFilter implements GlobalFilter, Ordered {
                         dataBuffer.read(bs);
                         String req = new String(bs, StandardCharsets.UTF_8);
                         currentSpan.tag(ContextHelper.TRACER_API_REQUEST, req);
+                        logger.debug("Gateway请求数据: {}", req);
                         dataBuffer.readPosition(0);
                     }
                     return Flux.just(dataBuffer);
@@ -136,6 +137,7 @@ public class GatewayFilter implements GlobalFilter, Ordered {
                                         currentSpan.tag(ContextHelper.TRACER_API_RESULT, String.valueOf(result.getCode()));
                                     }
                                 }
+                                logger.debug("Gateway响应数据: {}", repJson);
                                 dataBuffer.readPosition(0);
                             }
                             return Flux.just(dataBuffer);

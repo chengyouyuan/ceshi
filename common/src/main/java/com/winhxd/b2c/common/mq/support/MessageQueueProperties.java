16d4afe8ba8507020ece1bcab6c917c0fe3a8b9d
lixiaodong <lixiaodong@winchannel.net>
2018-08-15 15:37:02 +0800

    调整MQ配置方式

---
+++ common/src/main/java/com/winhxd/b2c/common/mq/support/MessageQueueProperties.java
@@ -0,0 +1,40 @@
+package com.winhxd.b2c.common.mq.support;
+
+public class MessageQueueProperties {
+    private String address;
+    private String username;
+    private String password;
+    private String virtualHost;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVirtualHost() {
+        return virtualHost;
+    }
+
+    public void setVirtualHost(String virtualHost) {
+        this.virtualHost = virtualHost;
+    }
+}

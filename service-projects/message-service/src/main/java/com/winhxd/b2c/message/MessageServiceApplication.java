ad936a367ce615a935e073f4bd98e8a5f7609ddb
chengyouyuan <984415140@qq.com>
2018-08-15 10:37:36 +0800

    去掉message-service中的定时任务，添加支持eurka注解，修改请求分享二维码和配置信息的接口参数信息

--- service-projects/message-service/src/main/java/com/winhxd/b2c/message/MessageServiceApplication.java
+++ service-projects/message-service/src/main/java/com/winhxd/b2c/message/MessageServiceApplication.java
@@ -6,15 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.winhxd.b2c.message.dao")
 @ComponentScan(basePackages = "com.winhxd.b2c")
-@EnableScheduling
 @Import(MicroServiceConfig.class)
+@EnableFeignClients(basePackages = "com.winhxd.b2c.common.feign")
 public class MessageServiceApplication {
     private static final Logger log = LoggerFactory.getLogger(MessageServiceApplication.class);
 

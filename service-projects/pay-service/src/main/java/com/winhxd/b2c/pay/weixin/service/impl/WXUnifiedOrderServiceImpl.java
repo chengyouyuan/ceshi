0600d89cbf0ee53caf0dc61ffd1a36479159de7c
mahongliang <mahongliang@winchannel.net>
2018-08-15 14:11:32 +0800

    微信同意下单

---
+++ service-projects/pay-service/src/main/java/com/winhxd/b2c/pay/weixin/service/impl/WXUnifiedOrderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.winhxd.b2c.pay.weixin.service.impl;
+
+import com.winhxd.b2c.pay.weixin.service.WXUnifiedOrderService;
+
+/**
+ * 支付网关微信统一下单API实现
+ * @author mahongliang
+ * @date  2018年8月15日 下午2:10:57
+ * @Description 
+ * @version
+ */
+public class WXUnifiedOrderServiceImpl implements WXUnifiedOrderService {
+
+}

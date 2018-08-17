18b6457ba7c779991f06a14bb6ac4e3001af4f55
lixiaodong <lixiaodong@winchannel.net>
2018-08-17 09:44:32 +0800

    调整事件消息日志,redis配置方式

--- common/src/main/java/com/winhxd/b2c/common/mq/event/EventType.java
+++ common/src/main/java/com/winhxd/b2c/common/mq/event/EventType.java
@@ -2,6 +2,8 @@ package com.winhxd.b2c.common.mq.event;
 
 import com.winhxd.b2c.common.domain.order.model.OrderInfo;
 
+import java.util.Objects;
+
 /**
  * 事件类型
  *
@@ -11,11 +13,12 @@ public enum EventType {
     /**
      * 事件类型定义
      */
-    CUSTOMER_ORDER_CREATED(OrderInfo.class);
+    EVENT_CUSTOMER_ORDER_CREATED(OrderInfo.class);
 
     private Class<?> eventObjectClass;
 
     EventType(Class<?> eventObjectClass) {
+        Objects.requireNonNull(eventObjectClass);
         this.eventObjectClass = eventObjectClass;
     }
 

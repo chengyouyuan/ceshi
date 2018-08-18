c1550ad9f247f587214e6be68e54d5cb8d1a6c20
shijinxing <shijinxing@winchannel.net>
2018-08-18 19:19:31 +0800

    财务汇总接口

--- admin-server/src/main/java/com/winhxd/b2c/admin/module/pay/controller/FinancialManagerController.java
+++ admin-server/src/main/java/com/winhxd/b2c/admin/module/pay/controller/FinancialManagerController.java
@@ -1,10 +1,12 @@
 package com.winhxd.b2c.admin.module.pay.controller;
 
 import com.winhxd.b2c.common.domain.ResponseResult;
+import com.winhxd.b2c.common.domain.pay.condition.FinancialManagerCondition;
 import com.winhxd.b2c.common.domain.pay.vo.PayFinanceAccountDetailVO;
 import com.winhxd.b2c.common.feign.pay.FinancialManagerServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,8 @@ public class FinancialManagerController {
 	
 	/**出入帐汇总查询*/
 	@PostMapping("/queryStoreFinancialSummary")
-	ResponseResult<PayFinanceAccountDetailVO> queryStoreFinancialSummary(){
-		return managerServiceClient.queryStoreFinancialSummary();
+	ResponseResult<PayFinanceAccountDetailVO> queryStoreFinancialSummary(@RequestBody FinancialManagerCondition condition){
+		return managerServiceClient.queryStoreFinancialSummary(condition);
 	}
 	
 	/**财务入账明细*//*

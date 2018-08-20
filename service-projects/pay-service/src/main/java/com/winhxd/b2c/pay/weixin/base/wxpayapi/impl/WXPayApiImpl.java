d5c809b9b60ab239a40296e0b445a8bc42ab64b7
mahongliang <mahongliang@winchannel.net>
2018-08-21 01:50:40 +0800

    签名时，属性名为驼峰形式

--- service-projects/pay-service/src/main/java/com/winhxd/b2c/pay/weixin/base/wxpayapi/impl/WXPayApiImpl.java
+++ service-projects/pay-service/src/main/java/com/winhxd/b2c/pay/weixin/base/wxpayapi/impl/WXPayApiImpl.java
@@ -194,7 +194,7 @@ public class WXPayApiImpl implements WXPayApi {
 		String sign = null;
         try {
             //bean转map
-		    Map<String, String> reqData = XmlUtil.bean2MapUnderline2Hump(obj);
+		    Map<String, String> reqData = XmlUtil.bean2Map(obj);
 		    WXPayUtil.getLogger().warn("-----签名数据AAA------"+reqData);
         	//签名添加调用微信API入参
         	sign = WXPayUtil.generateSignature(reqData, config.getKey(), signType);

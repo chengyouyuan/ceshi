37872cef2c737e554d712e3e5cb71e4345cfe214
shijinxing <shijinxing@winchannel.net>
2018-08-20 17:54:18 +0800

    删除无用代码

--- service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/controller/CouponActivityController.java
+++ service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/controller/CouponActivityController.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -82,16 +81,6 @@ public class CouponActivityController implements CouponActivityServiceClient {
         result.setCode(BusinessCode.CODE_OK);
         result.setMessage("返回小店导入信息成功");
         return result;
-    }
-
-    private ImportResult<CouponActivityImportStoreVO> parseExcel(MultipartFile excel) {
-        ImportResult<CouponActivityImportStoreVO> importResult = null;
-        try {
-            importResult = ExcelUtil.importExcelVerify(excel.getInputStream(), CouponActivityImportStoreVO.class);
-        } catch (Exception e) {
-            throw new BusinessException(BusinessCode.CODE_1001, e);
-        }
-        return importResult;
     }
 
     /**

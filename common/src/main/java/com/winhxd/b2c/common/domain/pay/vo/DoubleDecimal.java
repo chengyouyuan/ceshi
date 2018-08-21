2f36a1ca1509cd21e2fa6858741fe150b5909fbc
suizhen <suizhen@winchannel.net>
2018-08-21 13:58:30 +0800

    结算页面查询条件配置

---
+++ common/src/main/java/com/winhxd/b2c/common/domain/pay/vo/DoubleDecimal.java
@@ -0,0 +1,13 @@
+package com.winhxd.b2c.common.domain.pay.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class DoubleDecimal {
+
+    private BigDecimal start;
+
+    private BigDecimal end;
+}

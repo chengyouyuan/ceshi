0c7bc45e63e062a7f575ba979cae72060ad63aa1
yuluyuan <yuluyuan@winchannel.net>
2018-08-21 13:22:13 +0800

    因为collections4包下的CollectionUtils

--- service-projects/pay-service/src/main/java/com/winhxd/b2c/pay/weixin/service/impl/WXDownloadBillServiceImpl.java
+++ service-projects/pay-service/src/main/java/com/winhxd/b2c/pay/weixin/service/impl/WXDownloadBillServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

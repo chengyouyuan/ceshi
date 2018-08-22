f0a70e1980a74f613daf83b43d688b1a60d9f892
jujinbiao <jujinbiao@winchannel.net>
2018-08-22 14:50:28 +0800

    消息服务 后台消息管理列表权限

--- admin-server/src/main/java/com/winhxd/b2c/admin/module/message/controller/MessageBatchPushController.java
+++ admin-server/src/main/java/com/winhxd/b2c/admin/module/message/controller/MessageBatchPushController.java
@@ -1,6 +1,7 @@
 package com.winhxd.b2c.admin.module.message.controller;
 
 import com.winhxd.b2c.admin.common.context.UserManager;
+import com.winhxd.b2c.admin.common.security.annotation.CheckPermission;
 import com.winhxd.b2c.common.constant.BusinessCode;
 import com.winhxd.b2c.common.domain.PagedList;
 import com.winhxd.b2c.common.domain.ResponseResult;
@@ -8,6 +9,7 @@ import com.winhxd.b2c.common.domain.message.condition.MessageBatchPushCondition;
 import com.winhxd.b2c.common.domain.message.dto.MessageBatchPushDTO;
 import com.winhxd.b2c.common.domain.message.model.MessageBatchPush;
 import com.winhxd.b2c.common.domain.message.vo.MessageBatchPushVO;
+import com.winhxd.b2c.common.domain.system.security.enums.PermissionEnum;
 import com.winhxd.b2c.common.domain.system.user.vo.UserInfo;
 import com.winhxd.b2c.common.feign.message.MessageServiceClient;
 import io.swagger.annotations.Api;
@@ -43,6 +45,7 @@ public class MessageBatchPushController {
     @ApiOperation(value = "根据条件查询手动推送消息的分页数据信息", notes = "根据条件查询手动推送消息的分页数据信息")
     @ApiResponses({@ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部错误,查询手动推送消息列表数据失败"), @ApiResponse(code = BusinessCode.CODE_OK, message = "操作成功")})
     @PostMapping(value = "/findMessageBatchPushPageInfo")
+    @CheckPermission(PermissionEnum.MESSAGE_MANAGEMENT_LIST)
     public ResponseResult<PagedList<MessageBatchPushVO>> findMessageBatchPushPageInfo(@RequestBody MessageBatchPushCondition condition) {
         ResponseResult<PagedList<MessageBatchPushVO>> responseResult = messageServiceClient.findMessageBatchPushPageInfo(condition);
         return responseResult;
@@ -51,6 +54,7 @@ public class MessageBatchPushController {
     @ApiOperation(value = "新增手动推送消息", notes = "新增手动推送消息")
     @ApiResponses({@ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部错误,新增手动推送消息失败"), @ApiResponse(code = BusinessCode.CODE_OK, message = "操作成功")})
     @PostMapping(value = "/addBatchPush")
+    @CheckPermission(PermissionEnum.MESSAGE_MANAGEMENT)
     public ResponseResult addBatchPush(@RequestBody MessageBatchPushDTO messageBatchPushDTO) {
         logger.info("{} - 新增手动推送消息, 参数：messageBatchPushDTO={}", MODULE_NAME, messageBatchPushDTO);
         UserInfo userInfo = UserManager.getCurrentUser();
@@ -69,6 +73,7 @@ public class MessageBatchPushController {
     @ApiOperation(value = "获取手动推送消息", notes = "获取手动推送消息")
     @ApiResponses({@ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部错误,获取手动推送消息失败"), @ApiResponse(code = BusinessCode.CODE_OK, message = "操作成功")})
     @GetMapping(value = "/getBatchPush/{id}")
+    @CheckPermission(PermissionEnum.MESSAGE_MANAGEMENT)
     public ResponseResult<MessageBatchPush> getBatchPush(@PathVariable("id") Long id) {
         return messageServiceClient.getBatchPush(id);
     }
@@ -76,6 +81,7 @@ public class MessageBatchPushController {
     @ApiOperation(value = "修改手动推送消息", notes = "修改手动推送消息")
     @ApiResponses({@ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部错误,新增手动推送消息失败"), @ApiResponse(code = BusinessCode.CODE_OK, message = "操作成功")})
     @PostMapping(value = "/modifyBatchPush")
+    @CheckPermission(PermissionEnum.MESSAGE_MANAGEMENT)
     public ResponseResult modifyBatchPush(@RequestBody MessageBatchPushDTO messageBatchPushDTO) {
         logger.info("{} - 修改手动推送消息, 参数：messageBatchPushDTO={}", MODULE_NAME, messageBatchPushDTO);
         UserInfo userInfo = UserManager.getCurrentUser();
@@ -91,6 +97,7 @@ public class MessageBatchPushController {
     @ApiOperation(value = "删除手动推送消息", notes = "删除手动推送消息")
     @ApiResponses({@ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部错误,新增手动推送消息失败"), @ApiResponse(code = BusinessCode.CODE_OK, message = "操作成功")})
     @GetMapping(value = "/removeBatchPush/{id}")
+    @CheckPermission(PermissionEnum.MESSAGE_MANAGEMENT)
     public ResponseResult removeBatchPush(@PathVariable("id") Long id) {
         logger.info("{} - 删除手动推送消息, 参数：id={}", MODULE_NAME, id);
         return messageServiceClient.removeBatchPush(id);
@@ -99,6 +106,7 @@ public class MessageBatchPushController {
     @ApiOperation(value = "手动推送消息", notes = "手动推送消息")
     @ApiResponses({@ApiResponse(code = BusinessCode.CODE_1001, message = "服务器内部错误,手动推送消息失败"), @ApiResponse(code = BusinessCode.CODE_OK, message = "操作成功")})
     @GetMapping(value = "/batchPushMessage/{id}")
+    @CheckPermission(PermissionEnum.MESSAGE_MANAGEMENT)
     public ResponseResult batchPushMessage(@PathVariable("id") Long id) {
         logger.info("{} - 手动推送消息, 参数：id={}", MODULE_NAME, id);
         return messageServiceClient.batchPushMessage(id);

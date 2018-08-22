a3e562467c40bfcbb2871c1365cc0d7f4da9916b
wuyuanbao <wuyuanbao@winchannel.net>
2018-08-22 14:01:57 +0800

    菜单权限

--- common/src/main/java/com/winhxd/b2c/common/domain/system/security/enums/MenuEnum.java
+++ common/src/main/java/com/winhxd/b2c/common/domain/system/security/enums/MenuEnum.java
@@ -20,11 +20,18 @@ public enum MenuEnum {
 
 
     /**
+     * 商品管理
+     */
+    PRODUCT_MANAGEMENT("商品管理"),
+    PRODUCT_MANAGEMENT_STORE("门店商品管理"),
+    PRODUCT_MANAGEMENT_SUBMIT("门店提报商品管理"),
+    /**
      * 系统管理
      */
     SYSTEM_MANAGEMENT("系统管理", PermissionEnum.SYSTEM_MANAGEMENT),
     SYSTEM_MANAGEMENT_ROLE("角色管理", SYSTEM_MANAGEMENT, PermissionEnum.SYSTEM_MANAGEMENT_ROLE),
     SYSTEM_MANAGEMENT_USER("用户管理", SYSTEM_MANAGEMENT, PermissionEnum.SYSTEM_MANAGEMENT_USER);
+
 
 
     private String name;

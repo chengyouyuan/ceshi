3680c25ac247b000acb8b8e6c791b45c47a1ba35
lixiaodong <lixiaodong@winchannel.net>
2018-08-22 15:23:22 +0800

    恢复网关

--- base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/config/FilterConfig.java
+++ base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/config/FilterConfig.java
@@ -1,14 +1,14 @@
-package com.winhxd.b2c.gateway.config;
-
-import com.winhxd.b2c.common.context.version.VersionedZoneAvoidanceRule;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-
-    @Bean
-    public VersionedZoneAvoidanceRule versionedZoneAvoidanceRule() {
-        return new VersionedZoneAvoidanceRule();
-    }
-}
+//package com.winhxd.b2c.gateway.config;
+//
+//import com.winhxd.b2c.common.context.version.VersionedZoneAvoidanceRule;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    @Bean
+//    public VersionedZoneAvoidanceRule versionedZoneAvoidanceRule() {
+//        return new VersionedZoneAvoidanceRule();
+//    }
+//}

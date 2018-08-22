8b2bfe5c6cb2bc05599463760066070ce6f6dc1d
lixiaodong <lixiaodong@winchannel.net>
2018-08-22 21:57:20 +0800

    去除zipkin无用日志

--- base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/config/FilterConfig.java
+++ base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/config/FilterConfig.java
@@ -1,7 +1,10 @@
 package com.winhxd.b2c.gateway.config;
 
+import brave.http.HttpAdapter;
+import brave.http.HttpSampler;
 import com.winhxd.b2c.common.context.version.VersionedZoneAvoidanceRule;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.cloud.sleuth.instrument.web.ClientSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -14,5 +17,16 @@ public class FilterConfig {
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public VersionedZoneAvoidanceRule versionedZoneAvoidanceRule() {
         return new VersionedZoneAvoidanceRule();
+    }
+
+    @Bean(name = ClientSampler.NAME)
+    public HttpSampler skipHttpSampler() {
+        HttpSampler httpSampler = new HttpSampler() {
+            @Override
+            public <Req> Boolean trySample(HttpAdapter<Req, ?> httpAdapter, Req req) {
+                return false;
+            }
+        };
+        return httpSampler;
     }
 }

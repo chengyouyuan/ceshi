39b8a770abbc8a39cb94db5e57fefc683aa33ba8
lixiaodong <lixiaodong@winchannel.net>
2018-08-22 11:44:10 +0800

    文件上传不记录请求数据

--- base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/config/FilterConfig.java
+++ base-projects/gateway-server/src/main/java/com/winhxd/b2c/gateway/config/FilterConfig.java
@@ -1,24 +1,24 @@
-package com.winhxd.b2c.gateway.config;
-
-import com.winhxd.b2c.common.context.version.VersionedZoneAvoidanceRule;
-import com.winhxd.b2c.gateway.filter.VersionedLoadBalancerClientFilter;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.gateway.filter.LoadBalancerClientFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-//@ConditionalOnClass({LoadBalancerClient.class, RibbonAutoConfiguration.class, DispatcherHandler.class})
-//@AutoConfigureAfter(RibbonAutoConfiguration.class)
-public class FilterConfig {
-    @Bean
-//    @ConditionalOnBean(LoadBalancerClient.class)
-    public LoadBalancerClientFilter loadBalancerClientFilter(LoadBalancerClient client) {
-        return new VersionedLoadBalancerClientFilter(client);
-    }
-
-    @Bean
-    public VersionedZoneAvoidanceRule versionedZoneAvoidanceRule(){
-        return new VersionedZoneAvoidanceRule();
-    }
-}
+//package com.winhxd.b2c.gateway.config;
+//
+//import com.winhxd.b2c.common.context.version.VersionedZoneAvoidanceRule;
+//import com.winhxd.b2c.gateway.filter.VersionedLoadBalancerClientFilter;
+//import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+//import org.springframework.cloud.gateway.filter.LoadBalancerClientFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+////@ConditionalOnClass({LoadBalancerClient.class, RibbonAutoConfiguration.class, DispatcherHandler.class})
+////@AutoConfigureAfter(RibbonAutoConfiguration.class)
+//public class FilterConfig {
+//    @Bean
+////    @ConditionalOnBean(LoadBalancerClient.class)
+//    public LoadBalancerClientFilter loadBalancerClientFilter(LoadBalancerClient client) {
+//        return new VersionedLoadBalancerClientFilter(client);
+//    }
+//
+//    @Bean
+//    public VersionedZoneAvoidanceRule versionedZoneAvoidanceRule(){
+//        return new VersionedZoneAvoidanceRule();
+//    }
+//}

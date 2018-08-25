8d2fa0e664aba71feb743970ba1a09f6b00bdf75
lixiaodong <lixiaodong@winchannel.net>
2018-08-25 15:39:24 +0800

    添加客户端IP获取工具类

--- common/src/main/java/com/winhxd/b2c/common/util/IpUtil.java
+++ common/src/main/java/com/winhxd/b2c/common/util/IpUtil.java
@@ -11,19 +11,24 @@ import javax.servlet.http.HttpServletRequest;
  * 获取客户端Ip地址
  */
 public class IpUtil {
+    private static final String[] IP_HEADER_CANDIDATES = {"X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR", "HTTP_X_FORWARDED", "HTTP_X_CLUSTER_CLIENT_IP", "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR", "HTTP_FORWARDED", "HTTP_VIA", "REMOTE_ADDR"};
+    public static final String IP_UNKNOWN = "unknown";
+
     public static String getIpAddr() {
         String ipAddress = StringUtils.EMPTY;
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
         if (ServletRequestAttributes.class.isInstance(requestAttributes)) {
             HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-            ipAddress = request.getHeader("x-forwarded-for");
-            if (StringUtils.isBlank(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
-                ipAddress = request.getHeader("Proxy-Client-IP");
+            for (String header : IP_HEADER_CANDIDATES) {
+                String ip = request.getHeader(header);
+                if (StringUtils.isNotBlank(ip) && !IP_UNKNOWN.equalsIgnoreCase(ip)) {
+                    ipAddress = ip;
+                    break;
+                }
             }
-            if (StringUtils.isBlank(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
-                ipAddress = request.getHeader("WL-Proxy-Client-IP");
-            }
-            if (StringUtils.isBlank(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            if (StringUtils.isBlank(ipAddress) || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
                 ipAddress = request.getRemoteAddr();
             }
             if (ipAddress != null && ipAddress.indexOf(",") > 0) {

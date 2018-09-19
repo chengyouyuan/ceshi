9140d699a226a9565a5012fef7a31fd49bb75ee2
shijinxing <shijinxing@winchannel.net>
2018-09-19 17:23:42 +0800

    判断推券和领券活动的每类券，最多给门店推送100张或让门店可领100张。

--- service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/controller/CouponActivityController.java
+++ service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/controller/CouponActivityController.java
@@ -211,14 +211,6 @@ public class CouponActivityController implements CouponActivityServiceClient {
         }
         //判断推券和领券活动的每类券，最多给门店推送100张或让门店可领100张。
         Integer maxNum = 100;
-        if(condition.getCouponActivityTemplateList().get(0).getCustomerVoucherLimitType() == CouponActivityEnum.STORE_LIMITED.getCode()) {
-            if (condition.getCouponActivityTemplateList().get(0).getCustomerVoucherLimitNum()>maxNum){
-                throw new BusinessException(BusinessCode.CODE_503004);
-            }
-        }
-        if (condition.getCouponActivityTemplateList().get(0).getSendNum()>maxNum){
-            throw new BusinessException(BusinessCode.CODE_503004);
-        }
         //领券
         if(condition.getType() == CouponActivityEnum.PULL_COUPON.getCode()){
             if (StringUtils.isBlank(condition.getName()) || condition.getCouponActivityTemplateList() == null
@@ -241,6 +233,11 @@ public class CouponActivityController implements CouponActivityServiceClient {
                     && condition.getCouponActivityTemplateList().get(0).getCustomerVoucherLimitNum() == null){
                 throw new BusinessException(BusinessCode.CODE_1007);
             }
+            if(condition.getCouponActivityTemplateList().get(0).getCustomerVoucherLimitType() == CouponActivityEnum.STORE_LIMITED.getCode()) {
+                if (condition.getCouponActivityTemplateList().get(0).getCustomerVoucherLimitNum()>maxNum){
+                    throw new BusinessException(BusinessCode.CODE_503004);
+                }
+            }
             if (!CollectionUtils.isEmpty(condition.getCouponActivityTemplateList().get(0).getCouponActivityStoreCustomerList())){
                 if(condition.getCouponActivityTemplateList().get(0).getCouponActivityStoreCustomerList().get(0).getStoreId() == null){
                     throw new BusinessException(BusinessCode.CODE_1007);
@@ -259,6 +256,9 @@ public class CouponActivityController implements CouponActivityServiceClient {
                     || condition.getCouponActivityTemplateList().get(0).getEffectiveDays() == null
                     || condition.getCouponActivityTemplateList().get(0).getSendNum() == null){
                 throw new BusinessException(BusinessCode.CODE_1007);
+            }
+            if (condition.getCouponActivityTemplateList().get(0).getSendNum()>maxNum){
+                throw new BusinessException(BusinessCode.CODE_503004);
             }
         }
 

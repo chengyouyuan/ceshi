6d4d8c8a21e9c8c2d7379fae2c306400a19443dd
liuzhijia <liuzhijia@winchannel.net>
2018-09-28 13:16:40 +0800

    1.添加类

---
+++ common/src/main/java/com/winhxd/b2c/common/domain/promotion/vo/CouponPushVO.java
@@ -0,0 +1,228 @@
+package com.winhxd.b2c.common.domain.promotion.vo;
+
+import com.winhxd.b2c.common.domain.product.vo.BrandVO;
+import com.winhxd.b2c.common.domain.product.vo.ProductSkuVO;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ *
+ */
+public class CouponPushVO {
+
+    @ApiModelProperty(value = "活动id", required=true)
+    private Long activityId;
+    @ApiModelProperty(value = "优惠券id", required=true)
+    private Long templateId;
+    @ApiModelProperty(value = "优惠券发放id", required=true)
+    private Long sendId;
+    @ApiModelProperty(value = "适用规则id", required=true)
+    private Long applyId;
+    @ApiModelProperty(value = "是否有效 0 无效 1 有效", required=true)
+    private String status;
+    @ApiModelProperty(value = "优惠券金额计算方式1订单金额 2商品金额", required=true)
+    private String payType;
+    @ApiModelProperty(value = "坎级类型 1-满减/2-满赠", required=true)
+    private String type;
+    @ApiModelProperty(value = "满减优惠类型(1-金额/2-折扣）", required=true)
+    private String reducedType;
+    @ApiModelProperty(value = "满减金额", required=true)
+    private BigDecimal reducedAmt;
+    @ApiModelProperty(value = "优惠金额", required=true)
+    private BigDecimal discountedAmt;
+    @ApiModelProperty(value = "满减优惠折扣", required=true)
+    private BigDecimal discounted;
+    @ApiModelProperty(value = "优惠最大限额", required=true)
+    private BigDecimal discountedMaxAmt;
+    @ApiModelProperty(value = "优惠券适用对象类型 1、通用 2、品牌 3、品类4、商品", required=true)
+    private String applyRuleType;
+    @ApiModelProperty(value = "优惠券类型 1新用户注册 2老用户活动", required=true)
+    private String couponType;
+    @ApiModelProperty(value = "开始时间", required=true)
+    private String activityStart;
+    @ApiModelProperty(value = "结束时间", required=true)
+    private String activityEnd;
+    @ApiModelProperty(value = "优惠券状态状态(0-无效,1-已使用，2-未使用， 3-已过期,4-退回 5-快过期", required=true)
+    private String useStatus;
+    @ApiModelProperty(value = "优惠券是否可领取 0 已领取,1 可领取,2已领完", required=true)
+    private String receiveStatus;
+    @ApiModelProperty(value = "适用品牌信息")
+    private List<BrandVO> brands;
+//    @ApiModelProperty(value = "适用商品信息")
+//    private List<ProductSkuVO> products;
+    @ApiModelProperty(value = "优惠券数量", required=true)
+    private Integer couponNum;
+    @ApiModelProperty(value = "（品牌、品牌商）logo")
+    private String logoImg;
+
+
+    public List<BrandVO> getBrands() {
+        return brands;
+    }
+
+    public void setBrands(List<BrandVO> brands) {
+        this.brands = brands;
+    }
+
+    public Long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+
+    public Long getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(Long templateId) {
+        this.templateId = templateId;
+    }
+
+    public Long getSendId() {
+        return sendId;
+    }
+
+    public void setSendId(Long sendId) {
+        this.sendId = sendId;
+    }
+
+    public Long getApplyId() {
+        return applyId;
+    }
+
+    public void setApplyId(Long applyId) {
+        this.applyId = applyId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPayType() {
+        return payType;
+    }
+
+    public void setPayType(String payType) {
+        this.payType = payType;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getReducedType() {
+        return reducedType;
+    }
+
+    public void setReducedType(String reducedType) {
+        this.reducedType = reducedType;
+    }
+
+    public BigDecimal getReducedAmt() {
+        return reducedAmt;
+    }
+
+    public void setReducedAmt(BigDecimal reducedAmt) {
+        this.reducedAmt = reducedAmt;
+    }
+
+    public BigDecimal getDiscountedAmt() {
+        return discountedAmt;
+    }
+
+    public void setDiscountedAmt(BigDecimal discountedAmt) {
+        this.discountedAmt = discountedAmt;
+    }
+
+    public BigDecimal getDiscounted() {
+        return discounted;
+    }
+
+    public void setDiscounted(BigDecimal discounted) {
+        this.discounted = discounted;
+    }
+
+    public BigDecimal getDiscountedMaxAmt() {
+        return discountedMaxAmt;
+    }
+
+    public void setDiscountedMaxAmt(BigDecimal discountedMaxAmt) {
+        this.discountedMaxAmt = discountedMaxAmt;
+    }
+
+    public String getApplyRuleType() {
+        return applyRuleType;
+    }
+
+    public void setApplyRuleType(String applyRuleType) {
+        this.applyRuleType = applyRuleType;
+    }
+
+    public String getCouponType() {
+        return couponType;
+    }
+
+    public void setCouponType(String couponType) {
+        this.couponType = couponType;
+    }
+
+    public String getActivityStart() {
+        return activityStart;
+    }
+
+    public void setActivityStart(String activityStart) {
+        this.activityStart = activityStart;
+    }
+
+    public String getActivityEnd() {
+        return activityEnd;
+    }
+
+    public void setActivityEnd(String activityEnd) {
+        this.activityEnd = activityEnd;
+    }
+
+    public String getUseStatus() {
+        return useStatus;
+    }
+
+    public void setUseStatus(String useStatus) {
+        this.useStatus = useStatus;
+    }
+
+    public String getReceiveStatus() {
+        return receiveStatus;
+    }
+
+    public void setReceiveStatus(String receiveStatus) {
+        this.receiveStatus = receiveStatus;
+    }
+
+    public Integer getCouponNum() {
+        return couponNum;
+    }
+
+    public void setCouponNum(Integer couponNum) {
+        this.couponNum = couponNum;
+    }
+
+    public String getLogoImg() {
+        return logoImg;
+    }
+
+    public void setLogoImg(String logoImg) {
+        this.logoImg = logoImg;
+    }
+}

99c5763ace30be03194da8d440b2d702b9ed7bc8
liuzhijia <liuzhijia@winchannel.net>
2018-09-28 13:19:19 +0800

    1.添加推券dao和mapper

--- service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/dao/CouponPushCustomerMapper.java
+++ service-projects/promotion-service/src/main/java/com/winhxd/b2c/promotion/dao/CouponPushCustomerMapper.java
@@ -1,6 +1,9 @@
 package com.winhxd.b2c.promotion.dao;
 
 import com.winhxd.b2c.common.domain.promotion.model.CouponPushCustomer;
+import com.winhxd.b2c.common.domain.promotion.vo.CouponPushVO;
+
+import java.util.List;
 
 public interface CouponPushCustomerMapper {
     int deleteByPrimaryKey(Long id);
@@ -14,4 +17,25 @@ public interface CouponPushCustomerMapper {
     int updateByPrimaryKeySelective(CouponPushCustomer record);
 
     int updateByPrimaryKey(CouponPushCustomer record);
+
+    /**
+     * 查询优惠券发放给多少用户
+     * @param customerIds
+     * @return
+     */
+    int selectCouponStoreUser(List<Long> customerIds);
+
+    /**
+     * 查询优惠券推送给用户列表
+     * @param customerId
+     * @return
+     */
+    List<CouponPushVO> selectCouponPushCustomer(Long customerId);
+
+    /**
+     * 查询优惠券推送给门店-
+     * @param storeId
+     * @return
+     */
+    List<CouponPushVO> selectCouponPushStore(Long storeId);
 }

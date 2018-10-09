cba74b11a956c956f1d2159775abb348e4550bfc
liujing <liujing@winchannel.net>
2018-10-09 19:16:16 +0800

    去掉MQ连接

--- service-projects/detection-service/src/main/java/com/winhxd/b2c/detection/DetectionServiceApplication.java
+++ service-projects/detection-service/src/main/java/com/winhxd/b2c/detection/DetectionServiceApplication.java
@@ -3,12 +3,11 @@ package com.winhxd.b2c.detection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.amqp.RabbitMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {RabbitMetricsAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan(basePackages = "com.winhxd.b2c")
 @EnableFeignClients(basePackages = "com.winhxd.b2c.common.feign.detection")
 public class DetectionServiceApplication {

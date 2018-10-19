90fd1e4dd1a3fb098d548c3315e3311c215c569c
sunwenwu <sunwenwu@winchannel.net>
2018-10-19 09:39:15 +0800

    优惠券test

---
+++ service-projects/promotion-service/src/test/java/com/winhxd/b2c/promotion/service/CouponServiceTest.java
@@ -0,0 +1,16 @@
+package com.winhxd.b2c.promotion.service;
+
+import com.winhxd.b2c.promotion.PromotionServiceApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author: sunwenwu
+ * @Date: 2018/10/19 09：35
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PromotionServiceApplication.class)
+public class CouponServiceTest {
+}

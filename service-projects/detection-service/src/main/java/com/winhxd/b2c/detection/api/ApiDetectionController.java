a3421e822f39a4fcf24939b64a9e77373c14fe26
chenyanqi <m17610075813@163.com>
2018-10-23 11:33:08 +0800

    pay-service 优化

--- service-projects/detection-service/src/main/java/com/winhxd/b2c/detection/api/ApiDetectionController.java
+++ service-projects/detection-service/src/main/java/com/winhxd/b2c/detection/api/ApiDetectionController.java
@@ -1,23 +1,12 @@
 package com.winhxd.b2c.detection.api;
 
-import com.winhxd.b2c.common.constant.BusinessCode;
-import com.winhxd.b2c.common.domain.PagedList;
-import com.winhxd.b2c.common.domain.ResponseResult;
-import com.winhxd.b2c.common.domain.detection.condition.QuartzJobCondition;
-import com.winhxd.b2c.common.domain.detection.vo.QuartzJobVo;
-import com.winhxd.b2c.common.util.JsonUtil;
 import com.winhxd.b2c.detection.service.IQuartzJobService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**

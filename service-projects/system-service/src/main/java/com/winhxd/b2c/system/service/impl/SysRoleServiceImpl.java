97c1d8579c0fb638c6f824fb894e311898bd1823
liujing <liujing@winchannel.net>
2018-11-30 18:06:28 +0800

    前端过滤半选状态菜单

--- service-projects/system-service/src/main/java/com/winhxd/b2c/system/service/impl/SysRoleServiceImpl.java
+++ service-projects/system-service/src/main/java/com/winhxd/b2c/system/service/impl/SysRoleServiceImpl.java
@@ -4,16 +4,20 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.winhxd.b2c.common.domain.PagedList;
 import com.winhxd.b2c.common.domain.system.user.condition.SysRoleCondition;
+import com.winhxd.b2c.common.domain.system.user.model.SysPermission;
 import com.winhxd.b2c.common.domain.system.user.model.SysRole;
 import com.winhxd.b2c.common.domain.system.user.model.SysRolePermission;
 import com.winhxd.b2c.system.dao.SysRoleMapper;
 import com.winhxd.b2c.system.dao.SysRolePermissionMapper;
+import com.winhxd.b2c.system.service.SysPermissionService;
 import com.winhxd.b2c.system.service.SysRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author zhangzhengyang
@@ -27,7 +31,8 @@ public class SysRoleServiceImpl implements SysRoleService {
     private SysRoleMapper sysRoleMapper;
     @Autowired
     private SysRolePermissionMapper sysRolePermissionMapper;
-
+    @Autowired
+    private SysPermissionService sysPermissionService;
 
     @Override
     @Transactional(rollbackFor = Exception.class)
@@ -70,7 +75,18 @@ public class SysRoleServiceImpl implements SysRoleService {
 
     @Override
     public SysRole getSysRoleById(Long id) {
-        return sysRoleMapper.getSysRoleById(id);
+        SysRole sysRole = sysRoleMapper.getSysRoleById(id);
+        List<SysRolePermission> permissions = sysRole.getPermissions();
+        //前端过滤半选状态菜单
+        Map<String,SysRolePermission> grepMap = new HashMap();
+        for (SysRolePermission rolePermission : permissions) {
+            grepMap.put(rolePermission.getPermission(), rolePermission);
+        }
+        List<SysPermission> permissionList = sysPermissionService.list();
+        permissionList.stream().forEach(sysPermission -> {
+            grepPermison(sysPermission,grepMap,permissions);
+        });
+        return sysRole;
     }
 
     @Override
@@ -82,6 +98,20 @@ public class SysRoleServiceImpl implements SysRoleService {
     @Override
     public List<String> getPermissionsByUser(Long userId) {
         return sysRolePermissionMapper.selectPermissionByUserId(userId);
+    }
+
+    private void grepPermison(SysPermission sysPermission, Map<String,SysRolePermission> grepMap, List<SysRolePermission> permissions) {
+        List<SysPermission> children = sysPermission.getChildren();
+        if (children != null && children.size() > 0) {
+            children.stream().forEach(permission -> {
+                grepPermison(permission,grepMap,permissions);
+                if (null == grepMap.get(permission.getPermission())) {
+                    permissions.remove(grepMap.get(sysPermission.getPermission()));
+                    grepMap.remove(sysPermission.getPermission());
+                    return;
+                }
+            });
+        }
     }
 
 }

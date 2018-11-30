8615443fe8ffd2ceb9dfe98bc92a04b006ecdad4
chenyanqi <m17610075813@163.com>
2018-11-30 17:01:59 +0800

    超时未自提退款，延时时间改为 36小时

--- common/src/main/java/com/winhxd/b2c/common/constant/OrderOperateTime.java
+++ common/src/main/java/com/winhxd/b2c/common/constant/OrderOperateTime.java
@@ -7,17 +7,10 @@ public class OrderOperateTime {
      */
     public static final int ORDER_NEED_RECEIVE_TIME_BY_MILLISECONDS = 24 * 60 * 60 * 1000;
 
-//    /**
-//     * 客户自提 时间范围：36小时
-//     */
-//    public static final int ORDER_NEED_PICKUP_TIME_BY_MILLISECONDS = 36 * 60 * 60 * 1000;
-
-
     /**
      * 客户自提 时间范围：36小时
      */
-    public static final int ORDER_NEED_PICKUP_TIME_BY_MILLISECONDS = 3 * 60 * 1000;
-
+    public static final int ORDER_NEED_PICKUP_TIME_BY_MILLISECONDS = 36 * 60 * 60 * 1000;
 
     /**
      * 客户付款 时间范围：24小时
